91459bed22dbf82de4447eb552cfd38be79426cf
HopeBetrayer <me.at.the.wind@gmail.com>
2016-07-27 12:18:01 +0300

    added Rbac console controller which adds new roles

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=xdetalru_test',   //локальная база
+    'dsn' => 'mysql:host=localhost;dbname=xdetalru',   //локальная база
     //'dsn' => 'mysql:host=VH81.spaceweb.ru;port=3306;dbname=xdetalru_test',    //тестовая база на серве
     'username' => 'xdetalru_test',
-    'password' => 'q*nKLd6jdg',
+    'password' => 'jsg72K(3',
     'charset' => 'utf8',
 ];
